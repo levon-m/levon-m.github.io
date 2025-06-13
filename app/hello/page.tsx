2cc0eb329053459f622a38a519caecb401f7715d
Levon <levonpnduk@gmail.com>
2025-06-13 13:13:37 -0700

    colors

--- app/hello/page.tsx
+++ app/hello/page.tsx
@@ -2,17 +2,17 @@ import Link from 'next/link'
 
 export default function Home() {
   const links = [
-    { href: '/bio', text: 'bio' },
-    { href: '/projects', text: 'personal projects' },
-    { href: '/experience', text: 'work experience' },
-    { href: '/education', text: 'education' },
-    { href: '/contact', text: 'contact' },
-    { href: '/cv.pdf', text: 'download cv' },
+    { href: '/bio', text: '• bio' },
+    { href: '/projects', text: '• personal projects' },
+    { href: '/experience', text: '• work experience' },
+    { href: '/education', text: '• education' },
+    { href: '/contact', text: '• contact' },
+    { href: '/cv.pdf', text: '• download cv' },
   ]
 
   return (
     <main className="min-h-screen flex items-center">
-      <div className="max-w-2xl p-16 space-y-12 ml-[20%]">
+      <div className="max-w-2xl p-16 space-y-12 ml-[15%]">
         <h1 className="text-4xl name">Levon Melkonyan</h1>
         {links.map((link) => (
           <div key={link.href} className="text-xl">
