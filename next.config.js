4d4968664b764d222582a53624a749493ac64661
Levon <levonpnduk@gmail.com>
2025-06-13 10:20:48 -0700

    v2.1

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '',
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
-  distDir: 'out',
 }
 
 module.exports = nextConfig 
